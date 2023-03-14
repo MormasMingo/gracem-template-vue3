b3bf11655a60bfe25252c990c310d58854e172d6
Mormas <657560745@qq.com>
2023-03-14 17:44:39 +0800

    引入element-plus

---
+++ src/layouts/LayoutBasic.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-container>
+        <el-header>top</el-header>
+
+        <el-container>
+            <el-aside>side</el-aside>
+
+            <el-container>
+                <el-main>main</el-main>
+
+                <el-footer>footer</el-footer>
+            </el-container>
+        </el-container>
+    </el-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang=""></style>
